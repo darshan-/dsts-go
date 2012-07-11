d8a4c304cf7ce81d35da0109960706d63201e15b
Darshan-Josiah Barber <darshan@darshancomputing.com>
2012-07-11 14:52:03 -0700

    Added Xhtml page type.

--- dsts.go
+++ dsts.go
@@ -3,11 +3,13 @@ package dsts
 import (
 	"bytes"
 	"html/template"
+	"strings"
 )
 
 type page struct {
 	Title    string
 	Encoding string
+	Doctype string
 
 	styles   []string
 	scripts  []string
@@ -16,6 +18,10 @@ type page struct {
 }
 
 type Html5Page struct {
+	page
+}
+
+type XhtmlPage struct {
 	page
 }
 
@@ -37,11 +43,46 @@ var html5TemplStr = `<!DOCTYPE html>
 </html>
 `
 
+var xhtmlTemplStr = `<!DOCTYPE ` +
+`html PUBLIC "-//W3C//DTD XHTML 1.0 {{toUpper .Doctype}}//EN" ` +
+`"http://www.w3.org/TR/xhtml1/DTD/xhtml1-{{toLower .Doctype}}.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head profile="http://www.w3.org/2005/10/profile">
+    <title>{{.Title}}</title>
+    {{range .Styles}}` +
+      `<link rel="stylesheet" href="{{.}}" type="text/css" />
+    {{end}}` +
+    `{{range .Scripts}}` +
+      `<script type="text/javascript" src="{{.}}"></script>
+    {{end}}` +
+    `<meta http-equiv="Content-Type" content="text/html; charset={{.Encoding}}" />
+  </head>
+<body>
+{{.Content}}
+</body>
+</html>
+`
+
 func NewHtml5Page() *Html5Page {
 	p := new(Html5Page)
 
 	p.Encoding = "utf-8"
 	p.templ, _ = template.New("").Parse(html5TemplStr)
+
+	return p
+}
+
+func NewXhtmlPage() *XhtmlPage {
+	p := new(XhtmlPage)
+
+	funcMap := template.FuncMap{
+		"toUpper": strings.ToUpper,
+		"toLower": strings.ToLower,
+	}
+
+	p.Encoding = "utf-8"
+	p.Doctype  = "strict"
+	p.templ, _ = template.New("").Funcs(funcMap).Parse(xhtmlTemplStr)
 
 	return p
 }
