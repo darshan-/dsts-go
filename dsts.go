e9da3002b220900ee6ff947381322ee904330419
Darshan-Josiah Barber <darshan@darshancomputing.com>
2012-07-11 02:04:09 -0700

    This is called experimentation.

--- dsts.go
+++ dsts.go
@@ -4,18 +4,51 @@ import (
 	"bytes"
 )
 
-type page struct {
-	content bytes.Buffer
+type Page struct {
+	Title     string
+	content   bytes.Buffer
+	openPage  func() string
+	closePage func() string
+	openBody  func() string
+	closeBody func() string
 }
 
-type Html5Page struct {
-	page
+func NewHtml5Page() (*Page) {
+	p := new(Page)
+
+	p.openPage = func () string {
+		return `<!DOCTYPE html>
+<html>
+<head>
+<title>` + stripTags(p.Title) + `</title>
+<meta http-equiv="Content-Type" content="text/html; charset=#{encoding}" />
+`
 }
 
-func (p *page) Add(s string) {
+	p.closePage = func() string {
+		return "</html>\n"
+	}
+
+	p.openBody = func() string {
+		return "</head>\n<body>\n"
+	}
+
+	p.closeBody = func() string {
+		return "\n</body>\n"
+	}
+
+	return p
+}
+
+func (p *Page) Add(s string) {
 	p.content.WriteString(s)
 }
 
-func (p *page) String() string {
-	return p.content.String()
+func (p *Page) String() string {
+	return p.openPage() + p.openBody() + p.content.String() + p.closeBody() + p.closePage()
+}
+
+func stripTags(s string) string {
+//    s.gsub(/(<.*?>)/, "")
+	return s
 }
